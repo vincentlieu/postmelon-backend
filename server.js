a83d5310d2d6ed70df7672eb5fbb628d42e464a9
patricia <patriciapavia236@gmail.com>
2020-07-15 10:45:15 +1000

    POS-40 get all test route set up and already test it working

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ app.listen(PORT, () => console.group(`Server running on port ${PORT}`));
 // connect to database
 
 connectDB();
+
+// define routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
