9fb260f47ca68d35c2858ca5e19ff9669d99486a
patricia <patriciapavia236@gmail.com>
2020-07-16 14:53:49 +1000

    reorganize file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 mongoose = require("mongoose");
 const config = require("config");
 
-const db = config.get("mongoURI");
+const db = config.get("ATLAS_URI");
 
 const connectDB = async () => {
   try {
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const usersRouter = require("./routes/api/users");
 
 app.get("/", (req, res) => res.send("API RUNNING"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.group(`Server running on port ${PORT}`));
@@ -17,7 +19,7 @@ connectDB();
 
 // define routes
 
-app.use("/api/users", require("./routes/api/users"));
+app.use("/api/users", usersRouter);
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
