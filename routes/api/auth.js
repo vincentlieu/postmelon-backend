c2281aa8d7e0febdb832ee8f9dcc01d3601383b8
vincentlieu <v.lieu93@gmail.com>
2020-07-27 15:59:46 +1000

    Pass userID along with token in auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -70,7 +70,7 @@ router.post(
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
-          res.json({ token });
+          res.json({ token, userId: payload.user.id });
         }
       ); // need secret key from default.json
     } catch (err) {
